9a9ea2f44197c1d764546f572b08e8c6be97e665
Marc Catrisse <marc@catrisse.net>
2020-12-27 13:37:36 +0100

    kernels.cu working heat diffusion

--- part4/kernels.cu
+++ part4/kernels.cu
@@ -3,7 +3,11 @@
 #include <cuda.h>
 
 __global__ void gpu_Heat (float *h, float *g, int N) {
-
-	// TODO: kernel computation
-	//...
+	int i = threadIdx.y + blockDim.y * blockIdx.y;
+	int j = threadIdx.x + blockDim.x * blockIdx.x;
+	g[i*N+j]= 0.25 * (h[i*N + (j-1)]+  // left
+					     h[i*N  + (j+1)]+  // right
+				             h[(i-1)*N + j]+  // top
+				             h[(i+1)*N + j]); // bottom
+	
 }
